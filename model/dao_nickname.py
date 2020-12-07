b1abfca84c97bcb061eae54b440249f92e3e8813
gianlucca <gianluccadevigili@gmail.com>
2020-12-07 17:51:11 -0300

    Create dao_nickname.py

---
+++ model/dao_nickname.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+def create_connection(db_file):
+    conn = None
+
+    try:
+        conn = sqlite3.connect(db_file)
+        return
+    except sqlite3.Error as e:
+        return e
+    finally:
+        if conn:
+            conn.close()
