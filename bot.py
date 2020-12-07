776623c8dcba90baecced6dcca04bc6ae7915b0b
gianlucca <gianluccadevigili@gmail.com>
2020-12-07 00:02:25 -0300

    Add a sample test feature

--- bot.py
+++ bot.py
@@ -1,20 +1,24 @@
 import discord as d
 import private.app_data as my_app
-
-client = d.Client()
+from discord.ext.commands import Bot
 
 TOKEN = my_app.get_bot_token()
+bot = Bot(command_prefix="?")
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'Bot connected as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return message.channel.send('Adm corno!')
+    if message.content == 'test':
+        await message.channel.send('Bip bip bop 1 2 3, rotom dex on')
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
-client.run(TOKEN)
+# class MyClient(d.Client):
+#     async def on_ready(self):
+#         print('Logged on as {0}!'.format(self.user))
+#
+#     async def on_message(self, message):
+#         print('Message from {0.author}: {0.content}'.format(message))
+
+bot.run(TOKEN)
