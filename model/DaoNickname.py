1d978746d111f9b6daf66b14f6b192b8d69a3b43
gianlucca <gianluccadevigili@gmail.com>
2020-12-09 16:44:07 -0300

    Fix the insert bug and add select_nickname function

--- model/DaoNickname.py
+++ model/DaoNickname.py
@@ -16,37 +16,48 @@ class DaoNickname:
             if conn:
                 conn.close()
 
+    # This function is only useful when creating a new database, so I have commented it
+    '''
     def create_table(self):
         conn = None
 
         try:
             conn = sqlite3.connect(self.db_file)
             cur = conn.cursor()
+
             cur.execute("""CREATE TABLE IF NOT EXISTS nicknames(
                             id INTEGER PRIMARY KEY,
                             discord_nick TEXT, 
                             game_nick TEXT
                         );
                         """)
+
             self.close(conn)
-            print("tabela criada")
+            print("tabela criada com sucesso")
+
             return 1
         except sqlite3.Error as e:
             print(e)
             self.close(conn)
             return 0
+    '''
 
     def select_all(self):
         try:
             conn = sqlite3.connect(self.db_file)
             cur = conn.cursor()
+
             cur.execute("SELECT * FROM nicknames;")
+
             select = cur.fetchall()
             self.close(conn)
+
             return select
+
         except sqlite3.Error as e:
             print(e)
             self.close(conn)
+
             return 0
 
     def insert(self, discord_nick, game_nick):
@@ -55,15 +66,20 @@ class DaoNickname:
         try:
             conn = sqlite3.connect(self.db_file)
             cur = conn.cursor()
+
             cur.execute(f"""INSERT INTO nicknames (
                                 discord_nick,
                                 game_nick
                             ) 
                             VALUES (
-                                {discord_nick},
-                                {game_nick}
+                                '{discord_nick}',
+                                '{game_nick}'
                             );
                         """)
+            conn.commit()
+
+            conn.close()
+
             return 1
         except sqlite3.Error as e:
             print(e)
@@ -72,6 +88,25 @@ class DaoNickname:
             if conn:
                 conn.close()
 
+    def select_nickname(self, discord_nick):
+        try:
+            conn = sqlite3.connect(self.db_file)
+            cur = conn.cursor()
+
+            cur.execute(f"""SELECT discord_nick, game_nick 
+                        FROM nicknames 
+                        WHERE discord_nick = '{discord_nick}'
+                        ORDER BY id DESC
+                        LIMIT 1;""")
+
+            select = cur.fetchall()
+            self.close(conn)
+
+            return select
+        except sqlite3.Error as e:
+            print(e)
+            self.close(conn)
+            return 0
 
     @staticmethod
     def close(conn):
@@ -81,6 +116,6 @@ class DaoNickname:
 
 if __name__ == '__main__':
     c = DaoNickname()
-    # print(c.create_table())
-    # print(c.insert("teste", "game_nick"))
-    # print(c.select_all())
+    print(c.select_all())
+    print("--------")
+    print(c.select_nickname("Giraffes are Fake#5632"))
