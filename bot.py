6bd514afc991ad84393e9284e2593c245ee40efc
gianlucca <gianluccadevigili@gmail.com>
2020-12-13 12:44:36 -0300

    Refactor set_nick

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord as d
 import private.app_data as my_app
 from discord.ext.commands import Bot
-# from model.MessageEvent import
-from model.CommandEvent import CommandEvent
+# from model.CommandEvent import CommandEvent
+from model.DaoNickname import DaoNickname
 
 TOKEN = my_app.get_bot_token()
 bot = Bot(command_prefix="+")
@@ -13,11 +13,24 @@ async def on_ready():
     print(f'Bot connected as {bot.user}')
 
 
-@bot.command(name="oi", aliases=["olá", "ola"])
+@bot.command(name="oi", aliases=["olá", "ola", "bom dia", "boa tarde", "boa noite"])
 async def _hi(msg):
     await msg.channel.send(f"Olá {msg.author.mention}")
 
-    
+
+@bot.command(name="set-nick", aliases=["setnick"])
+async def _set_nick(msg, game_nick=None):
+    if game_nick:
+        dao = DaoNickname()
+        if dao.insert(msg.author, game_nick):
+            await msg.channel.send(f"{str(str(msg.author.mention))} seu nick foi regizztrado com sucesso :)! \n"
+                                   f"Ou será que eu deveria te chamar de **{game_nick}** agora? ")
+        else:
+            await msg.channel.send(f"Aconteceu o seguinte erro:\n```{self.error_message()}```\nPor favor, informe ")
+    else:
+        await msg.channel.send(f"Opzz, acho que não tem nenhum nick para eu registrar\nTente usar\n```+nick SeuNick```")
+
+
 # --------------------------------
 # class MyClient(d.Client):
 #     async def on_ready(self):
--- model/CommandEvent.py
+++ model/CommandEvent.py
@@ -3,59 +3,72 @@ from discord.ext.commands import Bot
 from model.DaoNickname import DaoNickname
 
 
-class CommandEvent:
-    def __init__(self, bot: Bot, message):
-        self.bot = bot
-        self.message = message
-        self.aux_lst = message.content.split(" ")
-        self.command = self.aux_lst[0]
-        self.args = self.aux_lst[1:]
+def set_nick(msg, game_nick):
+    dao = DaoNickname()
+    if dao.insert(msg.author, game_nick):
+        return (f"{str(str(msg.author.mention))} seu nick foi regizztrado com sucesso :)! \n"
+                f"Ou será que eu deveria te chamar de **{game_nick}** agora? ")
+    else:
+        return f"Aconteceu o seguinte erro:\n```{self.error_message()}```\nPor favor, informe "
 
-    # Main
-    def verify_message(self):
-        if self.command in self.greet_list():
-            return self.greet()
-
-        if self.command == str(self.bot.command_prefix + "set-nick"):
-            return self.set_nick(self.aux_lst[1])
-
-        if self.command == str(self.bot.command_prefix + "nick"):
-            return self.get_nick()
-
-    # Greeting Functions
-    def greet(self):
-        return str(self.message.content)[1:] + " " + str(str(self.message.author)[:-5])
-
-    def greet_list(self):
-        greet_list = ["bom-dia",
-                      "boa-tarde",
-                      "boa-noite",
-                      "oi"]
-        return [str(self.bot.command_prefix) + greet for greet in greet_list]
-
-    # Nickname Functions
-    def set_nick(self, game_nick):
-        dao = DaoNickname()
-        if dao.insert(self.message.author, game_nick):
-            return (f"{str(str(self.message.author)[:-5])} seu nick foi regizztrado com sucesso :)! \n"
-                   f"Ou será que eu deveria te chamar de **{game_nick}** agora? ")
-
-        else:
-            return self.error_message()
-
-    def get_nick(self):
-        dao = DaoNickname()
-        nickname = dao.select_nickname(self.message.author)
-        print(nickname)
-        if nickname:
-            return f"```Nickname: {nickname}```"
-        else:
-            return self.error_message()
 
     @staticmethod
     def under_construction(self):
         return "Oopzz! Parece que esta função ainda ezztá sendo dezzenvolvida bzz"
-
-    def error_message(self):
-        return ("Oopz, parece que *bip* aconteceu algum *bop erro ao realizar ezzta ação!\n"
-                    "Tente de novo ou reporte o erro ao desenvolvedor")
+#
+# class CommandEvent:
+#     def __init__(self, bot: Bot, message):
+#         self.bot = bot
+#         self.message = message
+#         self.aux_lst = message.content.split(" ")
+#         self.command = self.aux_lst[0]
+#         self.args = self.aux_lst[1:]
+#
+#     # Main
+#     def verify_message(self):
+#         if self.command in self.greet_list():
+#             return self.greet()
+#
+#         if self.command == str(self.bot.command_prefix + "set-nick"):
+#             return self.set_nick(self.aux_lst[1])
+#
+#         if self.command == str(self.bot.command_prefix + "nick"):
+#             return self.get_nick()
+#
+#     # Greeting Functions
+#     def greet(self):
+#         return str(self.message.content)[1:] + " " + str(str(self.message.author)[:-5])
+#
+#     def greet_list(self):
+#         greet_list = ["bom-dia",
+#                       "boa-tarde",
+#                       "boa-noite",
+#                       "oi"]
+#         return [str(self.bot.command_prefix) + greet for greet in greet_list]
+#
+#     # Nickname Functions
+#     def set_nick(self, game_nick):
+#         dao = DaoNickname()
+#         if dao.insert(self.message.author, game_nick):
+#             return (f"{str(str(self.message.author)[:-5])} seu nick foi regizztrado com sucesso :)! \n"
+#                    f"Ou será que eu deveria te chamar de **{game_nick}** agora? ")
+#
+#         else:
+#             return self.error_message()
+#
+#     def get_nick(self):
+#         dao = DaoNickname()
+#         nickname = dao.select_nickname(self.message.author)
+#         print(nickname)
+#         if nickname:
+#             return f"```Nickname: {nickname}```"
+#         else:
+#             return self.error_message()
+#
+#     @staticmethod
+#     def under_construction(self):
+#         return "Oopzz! Parece que esta função ainda ezztá sendo dezzenvolvida bzz"
+#
+#     def error_message(self):
+#         return ("Oopz, parece que *bip* aconteceu algum *bop erro ao realizar ezzta ação!\n"
+#                     "Tente de novo ou reporte o erro ao desenvolvedor")
