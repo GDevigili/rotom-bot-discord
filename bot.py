a3a572164bf317ca7c600234fa0d2a659f00341a
gianlucca <gianluccadevigili@gmail.com>
2020-12-15 17:48:05 -0300

    Delete a comment

--- bot.py
+++ bot.py
@@ -158,15 +158,4 @@ def error_msg(error):
            f"```\n" \
            f"Informe ao desenvolvedor para que ele possa ser resolvido"
 
-
-
-
-# --------------------------------
-# class MyClient(d.Client):
-#     async def on_ready(self):
-#         print('Logged on as {0}!'.format(self.user))
-#
-#     async def on_message(self, message):
-#         print('Message from {0.author}: {0.content}'.format(message))
-
 bot.run(TOKEN)
